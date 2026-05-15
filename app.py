9385b6199ee4240410b1ac1ecef755177091dd98
aniketahirwar02 <aniket.official40@gmail.com>
2026-05-15 13:43:44 +0530

    ''

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from products import products
 
 app = Flask(__name__)
 
@@ -6,21 +7,60 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 @app.route('/about_page')
 def about_page():
     return render_template('about_page.html')
+
 
 @app.route('/contact_page')
 def contact_page():
     return render_template('contact_page.html')
 
-@app.route('/comparsion_page')
-def comparsion_page():
-    return render_template('comparsion_page.html')
 
-@app.route('/result_page')
-def result_page():
-    return render_template('result_page.html')
+@app.route('/search')
+def search():
+
+    query = request.args.get('q')
+
+    filtered_products = []
+
+    for product in products:
+        if query.lower() in product['name'].lower():
+            filtered_products.append(product)
+
+    return render_template(
+        'result_page.html',
+        products=filtered_products,
+        query=query
+    )
+
+
+@app.route('/comparison_page/<int:product_id>')
+def comparison_page(product_id):
+
+    selected_product = None
+
+    for product in products:
+        if product['id'] == product_id:
+            selected_product = product
+            break
+
+    comparison_products = []
+
+    for product in products:
+        if product['name'] == selected_product['name']:
+            comparison_products.append(product)
+
+    best_price = min(comparison_products, key=lambda x: x['price'])
+
+    return render_template(
+        'comparison_page.html',
+        product=selected_product,
+        comparison_products=comparison_products,
+        best_price=best_price
+    )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ products.py
@@ -0,0 +1,44 @@
+products = [
+    {
+        "id": 1,
+        "name": "iPhone 15 Pro",
+        "price": 70999,
+        "store": "Amazon",
+        "image": "Iphone.jpg"
+    },
+    {
+        "id": 2,
+        "name": "iPhone 15 Pro",
+        "price": 69999,
+        "store": "Flipkart",
+        "image": "Iphone.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Samsung Galaxy S24",
+        "price": 65999,
+        "store": "Amazon",
+        "image": "samsung.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Samsung Galaxy S24",
+        "price": 64999,
+        "store": "Flipkart",
+        "image": "samsung.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Nike Air Max",
+        "price": 4999,
+        "store": "Myntra",
+        "image": "nike.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Nike Air Max",
+        "price": 5299,
+        "store": "Amazon",
+        "image": "nike.jpg"
+    }
+]
